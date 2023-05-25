e2015aff6ee15be92b89c84219071410c0b85301
KwonSunwon <kso00011147@gmail.com>
2023-05-25 23:07:07 +0900

    eClass 로그인 정보 입력 추가

--- proj02.py
+++ proj02.py
@@ -32,8 +32,7 @@ async def call_help(update, context):
 
 /selectsport : 스포츠 뉴스 종목 선택
 
-/eclassid <아이디> : eClass 아이디 등록
-/eclasspw <비밀번호> : eClass 비밀번호 등록""",
+/eclass <아이디> <비밀번호> : eClass 로그인 정보 입력""",
     )
 
 
@@ -45,7 +44,9 @@ async def introduce(update, context):
 
 명령어를 통해 eClass 로그인 정보,
 날씨 지역, 뉴스 검색 키워드, 스포츠 종목을
-선택하실 수 있습니다.""",
+선택하실 수 있습니다.
+
+/help 로 명령어 목록을 확인해 보세요!""",
     )
 
     if update.message.chat_id not in user_data:
@@ -164,6 +165,19 @@ async def call_selectlocate(update, context):
     )
 
 
+async def call_register(update, context):
+    user_data[str(update.message.chat_id)]["login_info"]["id"] = context.args[0]
+    user_data[str(update.message.chat_id)]["login_info"]["pw"] = context.args[1]
+    json_manager.save_user_data("user_data.json", user_data)
+
+    # eClass 아이디 유효성 확인하는 코드 추가하기
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="eClass 로그인 정보가 등록되었습니다.",
+    )
+
+
 if __name__ == "__main__":
     # 사용자 데이터
     user_data = json_manager.load_user_data("user_data.json")
@@ -175,6 +189,7 @@ if __name__ == "__main__":
     help_handler = CommandHandler("help", call_help)
     select_keyword_handler = CommandHandler("selectkeyword", call_selectkeyword)
     select_locate_handler = CommandHandler("selectlocate", call_selectlocate)
+    register_handler = CommandHandler("eclass", call_register)
 
     # eclassid_handler = CommandHandler("eclassid", call_eclassid)
     # eclasspw_handler = CommandHandler("eclasspw", call_eclasspw)
@@ -185,6 +200,7 @@ if __name__ == "__main__":
     application.add_handler(help_handler)
     application.add_handler(select_keyword_handler)
     application.add_handler(select_locate_handler)
+    application.add_handler(register_handler)
 
     select_handler = CommandHandler("selectsport", call_selectsport)
     callback_handler = CallbackQueryHandler(button_callback)
